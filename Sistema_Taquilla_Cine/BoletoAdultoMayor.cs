6c649fb7163dd864f5e1cdc30808c4428eb206df
AlejandroJS <juanalejandroshago@gmail.com>
2026-06-24 21:42:21 -0600

    Resolver conflicto conservando BoletoAdultoMayor

---
+++ Sistema_Taquilla_Cine/BoletoAdultoMayor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_Taquilla_Cine
+{
+    internal class BoletoAdultoMayor
+    {
+        public string Matricula { get; set; }
+        public float Precio { get; set; }
+
+        public BoletoAdultoMayor(decimal precioBase, string matricula) : base(precioBase)
+        {
+            Matricula = matricula;
+        }
+
+      
+
+    }
+}
+
